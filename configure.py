50ebe6b8d92baf07dd440350e2229f8c99a768b0
Grien25 <96752609+Grien25@users.noreply.github.com>
2025-07-14 15:17:37 -0700

    Expand assembler include paths

--- configure.py
+++ configure.py
@@ -170,8 +170,12 @@ config.asflags = [
     "-mgekko",
     "--strip-local-absolute",
     "-I include",
+    "-I DecompStart/include",
+    "-I DecompStart/src",
     f"-I build/{config.version}/include",
+    f"-I build/{config.version}/src",
     f"--defsym BUILD_VERSION={version_num}",
+    f"--defsym VERSION_{config.version}",
 ]
 config.ldflags = [
     "-fp hardware",
@@ -212,7 +216,9 @@ cflags_base = [
     "-multibyte",  # For Wii compilers, replace with `-enc SJIS`
     "-i include",
     "-i DecompStart/include",
+    "-i DecompStart/src",
     f"-i build/{config.version}/include",
+    f"-i build/{config.version}/src",
     f"-DBUILD_VERSION={version_num}",
     f"-DVERSION_{config.version}",
 ]
