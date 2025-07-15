f181933f8e88254a80dd018da4c43d7a0e18a578
Grien25 <96752609+Grien25@users.noreply.github.com>
2025-07-14 22:46:01 -0700

    Update Runtime.s

--- DecompStart/fromgame/asm/C++ Runtime/Runtime.s
+++ DecompStart/fromgame/asm/C++ Runtime/Runtime.s
@@ -4,22 +4,6 @@
 # Combined runtime assembly
 .text
 .balign 4
-.fn __fini_cpp_exceptions, global
-/* 8022897C 00223D7C  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80228980 00223D80  7C 08 02 A6 */	mflr r0
-/* 80228984 00223D84  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80228988 00223D88  80 6D 80 58 */	lwz r3, fragmentID@sda21(r0)
-/* 8022898C 00223D8C  2C 03 FF FE */	cmpwi r3, -0x2
-/* 80228990 00223D90  41 82 00 10 */	beq .L_802289A0
-/* 80228994 00223D94  48 00 00 69 */	bl __unregister_fragment
-/* 80228998 00223D98  38 00 FF FE */	li r0, -0x2
-/* 8022899C 00223D9C  90 0D 80 58 */	stw r0, fragmentID@sda21(r0)
-.L_802289A0:
-/* 802289A0 00223DA0  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 802289A4 00223DA4  7C 08 03 A6 */	mtlr r0
-/* 802289A8 00223DA8  38 21 00 10 */	addi r1, r1, 0x10
-/* 802289AC 00223DAC  4E 80 00 20 */	blr
-.endfn __fini_cpp_exceptions
 .fn __init_cpp, local
 /* 80276AC0 00271EC0  94 21 FF F0 */	stwu r1, -0x10(r1)
 /* 80276AC4 00271EC4  7C 08 02 A6 */	mflr r0
@@ -42,24 +26,6 @@
 /* 80276B00 00271F00  38 21 00 10 */	addi r1, r1, 0x10
 /* 80276B04 00271F04  4E 80 00 20 */	blr
 .endfn __init_cpp
-.fn __init_cpp_exceptions, global
-/* 80228940 00223D40  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80228944 00223D44  7C 08 02 A6 */	mflr r0
-/* 80228948 00223D48  90 01 00 14 */	stw r0, 0x14(r1)
-/* 8022894C 00223D4C  80 0D 80 58 */	lwz r0, fragmentID@sda21(r0)
-/* 80228950 00223D50  2C 00 FF FE */	cmpwi r0, -0x2
-/* 80228954 00223D54  40 82 00 18 */	bne .L_8022896C
-/* 80228958 00223D58  3C 60 80 00 */	lis r3, _eti_init_info@ha
-/* 8022895C 00223D5C  7C 44 13 78 */	mr r4, r2
-/* 80228960 00223D60  38 63 74 0C */	addi r3, r3, _eti_init_info@l
-/* 80228964 00223D64  48 00 00 4D */	bl __register_fragment
-/* 80228968 00223D68  90 6D 80 58 */	stw r3, fragmentID@sda21(r0)
-.L_8022896C:
-/* 8022896C 00223D6C  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80228970 00223D70  7C 08 03 A6 */	mtlr r0
-/* 80228974 00223D74  38 21 00 10 */	addi r1, r1, 0x10
-/* 80228978 00223D78  4E 80 00 20 */	blr
-.endfn __init_cpp_exceptions
 .fn __init_data, local
 /* 80006560 00002660  94 21 FF E0 */	stwu r1, -0x20(r1)
 /* 80006564 00002664  7C 08 02 A6 */	mflr r0
